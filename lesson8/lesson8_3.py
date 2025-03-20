15cbb3e952fd124ef90b032d69ac92f44e5cfddc
wu4fmp6 <eugenewang312@gmail.com>
2025-03-20 23:44:48 +0000

    modify

---
+++ lesson8/lesson8_3.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_csv('上市公司資料.csv')
+df1 = df.dropna()
+df2 = df1.reindex(columns=['公司代號','出表日期','公司名稱','產業別','營業收入-當月營收','營業收入-上月營收'])
+df3 = df2.rename(columns={
+    '營業收入-上月營收':'上月營收',
+    '營業收入-當月營收':'當月營收'
+    })
+print(df3)
